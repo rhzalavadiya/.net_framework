0d49f8be6a581fe79e2f505f08bb6ed1fffc9e9b
Rupali Zalavadiya <148213539+rhzalavadiya@users.noreply.github.com>
2024-08-03 15:23:59 +0530

    Add files via upload

---
+++ CustomerBill/CustomerBill/Customer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CustomerBill
+{
+    internal class Customer
+    {
+        private String _cname;
+        private int _unit;
+        private float _TotalBill;
+        private String _output;
+        public void SetName(String cname)
+        {
+            _cname = cname;
+        }
+        public int Units
+        {
+            set
+            {
+                _unit = value;
+            }
+        }
+        public void CalculateTotalBill()
+        {
+            _TotalBill = 0;
+            if (_unit <= 100) 
+            {
+                _TotalBill = _unit * 2.5f;
+                _output=_unit.ToString()+"* 2.5 = "+ _TotalBill.ToString()+"\n";
+            }
+            else  if (_unit <= 300)
+            {
+                _TotalBill = 250 + (_unit - 100) * 3.5f;
+                _output = "100 * 2.5 = 250\n" +(_unit-100).ToString()+" * 3.5 = "+ _TotalBill.ToString() + "\n";
+
+            }
+            else if (_unit > 300) 
+            {
+                _TotalBill = 250 + 700 + (_unit - 300) * 4.5f;
+                _output = "100 * 2.5 = 250\n200 * 2.5 = 700 \n" + (_unit - 300).ToString() + " * 4.5 = " + _TotalBill.ToString() + "\n";
+            }
+        }
+        public Tuple<String, int,String, float> GetData()
+        {
+            return new Tuple<String, int, String,float>(_cname, _unit,_output,_TotalBill);
+        }
+
+    }
+}
---
+++ emp_salary_bonus/emp_salary_bonus/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emp_salary_bonus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            Console.Write("Enter Name : - ");
+            String name=Console.ReadLine();
+
+            Console.Write("Enter Salary : - ");
+            int salary = Convert.ToInt32(Console.ReadLine());
+            emp.SetName(name);
+            emp.Salary = salary;
+
+            emp.CalculateBonus();
+            emp.CalculateNetSalary();
+
+            Tuple<String,int,float,float>result=emp.GetData();
+            Console.WriteLine("Name : - "+result.Item1);
+            Console.WriteLine("Salary : - " + result.Item2);
+            Console.WriteLine("Bonus : - " + result.Item3);
+            Console.WriteLine("Net Salary : - " + result.Item4);
+            Console.ReadKey();
+        }
+    }
+}
